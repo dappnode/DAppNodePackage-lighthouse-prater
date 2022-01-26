90c5007a60ac2609f38811931db26a685e5f61bc
pablo <mendez4a@gmail.com>
2022-01-26 17:36:54 +0100

    fix entrypoint
    Pending to convert string[] to bash array

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,6 +1,14 @@
-#! /bin/bash
+#!/bin/bash
+
+# This script does the following:
+# 1. Fetches the public keys from the web3signer API
+# 2. Checks if the public keys are valid
+# 3. Removes and creates again the validator_definitions.yml file
+# 4. Appends to the validator_definitions.yml file the public keys
+# 5. Starts the validator
 
 PUBLIC_KEYS=""
+VALIDATORS_FILE="/root/.lighthouse/validators/validator_definitions.yml"
 
 # Get public keys from API keymanager:
 # - Endpoint: http://web3signer.web3signer-prater.dappnode:9000/api/v1/eth2/publicKeys
@@ -11,15 +19,15 @@ function get_public_keys() {
         --max-time 10 \
         --retry 5 \
         --retry-delay 0 \
-        --retry-max-time 40
+        --retry-max-time 40 \
         "${HTTP_WEB3PROVIDER}/api/v1/eth2/publicKeys") \
-        || { echo "Failed to get public keys from API keymanager endpoint /api/v1/eth2/publicKeys"; exit 1}
+    echo "Found public keys: $PUBLIC_KEYS"
 }
 
 # Creates the validator_definitions.yml file for each public key
 # - Parameter: string[] publicKeys
 # - Docs: https://lighthouse-book.sigmaprime.io/validator-web3signer.html
-# - FORMAT:
+# - FORMAT for each new pubkey:
 # - enabled: true
 #   voting_public_key: "0xa5566f9ec3c6e1fdf362634ebec9ef7aceb0e460e5079714808388e5d48f4ae1e12897fed1bea951c17fa389d511e477"
 #   type: web3signer
@@ -27,11 +35,8 @@ function get_public_keys() {
 #   root_certificate_path: /home/paul/my-certificates/my-remote-signer.pem
 function write_validator_definitions() {
     for PUBLIC_KEY in ${PUBLIC_KEYS}; do
-        echo "enabled: true" >> /root/.lighthouse/validators/validator_definitions_${PUBLIC_KEY}.yml
-        echo "voting_public_key: \"${PUBLIC_KEY}\"" >> /root/.lighthouse/validators/validator_definitions_${PUBLIC_KEY}.yml
-        echo "type: web3signer" >> /root/.lighthouse/validators/validator_definitions_${PUBLIC_KEY}.yml
-        echo "url: \"${HTTP_WEB3PROVIDER}\"" >> /root/.lighthouse/validators/validator_definitions_${PUBLIC_KEY}.yml
-        # echo "root_certificate_path: /root/.lighthouse/validators/root_certificates/root_certificate.pem" >> /root/.lighthouse/validators/validator_definitions_${PUBLIC_KEY}.yml
+        echo "Adding public key: $PUBLIC_KEY"
+        echo "- enabled: true\n  voting_public_key: \"${PUBLIC_KEY}\"\n  type: web3signer\n  url: \"${HTTP_WEB3PROVIDER}\"" >> ${VALIDATORS_FILE}
     done
 }
 
@@ -43,13 +48,14 @@ function write_validator_definitions() {
 get_public_keys
 
 # Check public keys is not empty
-if [ -z "${PUBLIC_KEYS}" ]; then
-    echo "No public keys found in API keymanager endpoint /api/v1/eth2/publicKeys"
-    exit 1
-fi
+[ -z "${PUBLIC_KEYS}" ] && { echo "No public keys found in API keymanager endpoint /api/v1/eth2/publicKeys"; exit 1; }
 
-# Remove validator_definitions.yml files
-rm -f /root/.lighthouse/validators/*
+# Remove validator_definitions.yml file
+[ -f "${VALIDATORS_FILE}" ] && rm -rf "${VALIDATORS_FILE}"
+
+# Create validators file if not exist
+[ ! -d "/root/.lighthouse/validators" ] && mkdir -p /root/.lighthouse/validators
+[ ! -f "${VALIDATORS_FILE}" ] && touch "${VALIDATORS_FILE}"
 
 # Write validator_definitions.yml files
 write_validator_definitions

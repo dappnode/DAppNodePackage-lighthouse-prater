14d25c422584a8f2af2fad0e909d3ac505459c5d
pablo <mendez4a@gmail.com>
2022-01-26 14:25:40 +0100

    define new entrypoint

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,8 +1,58 @@
 #! /bin/bash
 
-set -e
+PUBLIC_KEYS=""
 
-sed -i 's/DEFAULT_VALIDATOR_PUBLIC_KEY/'"${DEFAULT_VALIDATOR_PUBLIC_KEY}"'/' /root/.lighthouse/validators/validator_definitions.yml
+# Get public keys from API keymanager:
+# - Endpoint: http://web3signer.web3signer-prater.dappnode:9000/api/v1/eth2/publicKeys
+# - Returns: string[]
+function get_public_keys() {
+    PUBLIC_KEYS=$(curl -X GET \
+        -H "Content-Type: application/json" \
+        --max-time 10 \
+        --retry 5 \
+        --retry-delay 0 \
+        --retry-max-time 40
+        "${HTTP_WEB3PROVIDER}/api/v1/eth2/publicKeys") \
+        || { echo "Failed to get public keys from API keymanager endpoint /api/v1/eth2/publicKeys"; exit 1}
+}
+
+# Creates the validator_definitions.yml file for each public key
+# - Parameter: string[] publicKeys
+# - Docs: https://lighthouse-book.sigmaprime.io/validator-web3signer.html
+# - FORMAT:
+# - enabled: true
+#   voting_public_key: "0xa5566f9ec3c6e1fdf362634ebec9ef7aceb0e460e5079714808388e5d48f4ae1e12897fed1bea951c17fa389d511e477"
+#   type: web3signer
+#   url: "https://my-remote-signer.com:1234"
+#   root_certificate_path: /home/paul/my-certificates/my-remote-signer.pem
+function write_validator_definitions() {
+    for PUBLIC_KEY in ${PUBLIC_KEYS}; do
+        echo "enabled: true" >> /root/.lighthouse/validators/validator_definitions.yml
+        echo "voting_public_key: \"${PUBLIC_KEY}\"" >> /root/.lighthouse/validators/validator_definitions.yml
+        echo "type: web3signer" >> /root/.lighthouse/validators/validator_definitions.yml
+        echo "url: \"${HTTP_WEB3PROVIDER}\"" >> /root/.lighthouse/validators/validator_definitions.yml
+        # echo "root_certificate_path: /root/.lighthouse/validators/root_certificates/root_certificate.pem" >> /root/.lighthouse/validators/validator_definitions.yml
+    done
+}
+
+########
+# MAIN #
+########
+
+# Get public keys from API keymanager
+get_public_keys
+
+# Check public keys is not empty
+if [ -z "${PUBLIC_KEYS}" ]; then
+    echo "No public keys found in API keymanager endpoint /api/v1/eth2/publicKeys"
+    exit 1
+fi
+
+# Remove validator_definitions.yml files
+rm -f /root/.lighthouse/validators/*
+
+# Write validator_definitions.yml files
+write_validator_definitions
 
 exec lighthouse \
     --debug-level $DEBUG_LEVEL \
